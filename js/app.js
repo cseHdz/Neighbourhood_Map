a42507e4a8d44af4f9a83f6bc9643cf1eeaa0dbf
Carlos Hernandez <carlos.hdz@me.com>
2018-03-29 12:41:50 -0400

    Added VM and initial handling of markers GMap

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
-var Location = function(data){
-    this.
+// Create a static list of markers
+var initialMarkers = [
+          {title: 'CN Tower', location: {lat: 43.6425662, lng: -79.3892455}},
+          {title: 'CF Eaton Centre', location: {lat: 43.6544421, lng: -79.3828881}},
+          {title: 'Nathan Phillips Square', location: {lat: 43.6525524, lng: -79.3857005}},
+          {title: 'Air Canada Centre', location: {lat: 43.64347, lng: -79.3812876}},
+          {title: 'TIFF Bell Lightbox', location: {lat: 43.6465334, lng: -79.3925969}}];
+
+// Define the model for marker
+var marker = function(data) {
+  this.title = data.title;
+  this.location = new Location(data.location);
+}
+
+// Define the model for location
+var location = function(data){
+  this.lat = data.lat;
+  this.lng = data.lng;
+}
+
+// Define the ViewModel
+var ViewModel = function() {
+  var self = this;
+  this.markerList = ko.observableArray([]);
+
+  initialMarkers.forEach(function(marker){
+    self.markerList.push(new Marker(marker))
+  })
 }
--- js/g_maps_script.js
+++ js/g_maps_script.js
@@ -12,28 +12,99 @@ function initMap() {
     center: {lat: 43.650059, lng: -79.381629},
     zoom: 13
   });
+
+  // TODO: Implement Search box
+  // Create searchbox to enable filtering by text
+  //var searchBox = new google.maps.places.SearchBox(
+      //document.getElementById('marker-search'));
+  // Bias the searchbox to within the bounds of the map.
+  //searchBox.setBounds(map.getBounds());
+
+  // Define default and highlighted marker styles
+  var defaultIcon = makeMarkerIcon('#082d68');
+  var highlightedIcon = makeMarkerIcon('#ff0000');
+
+  // InfoWindow to display marker details
+  var largeInfowindow = new google.maps.InfoWindow();
+
+  // Initialize the locations for Neighbourhood.
+  for (var i = 0; i < initialMarkers.length; i++) {
+      // Get the location dictionary from the marker .
+      var location = initialMarkers[i].location;
+      var title = initialMarkers[i].title;
+      // Create a marker per location, and push into markers array.
+      var marker = new google.maps.Marker({
+        position: location,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        icon: defaultIcon,
+        id: i
+      });
+
+      // Create an onclick event to open the large infowindow at each marker.
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+      });
+      // Two event listeners - one for mouseover, one for mouseout,
+      // to change the style when mouse hovers and leaves the marker area.
+      marker.addListener('mouseover', function() {
+        this.setIcon(highlightedIcon);
+      });
+      marker.addListener('mouseout', function() {
+        this.setIcon(defaultIcon);
+      });
+
+      document.getElementById('show-markers').addEventListener('click', funtion(){
+          toggleMarkers('show')
+      });
+
+      document.getElementById('show-markers').addEventListener('click', funtion(){
+          toggleMarkers('hide')
+      });
+  }
 }
 
-// TODO: Implement Search box
-// Create searchbox to enable filtering by text
-//var searchBox = new google.maps.places.SearchBox(
-    //document.getElementById('marker-search'));
-// Bias the searchbox to within the bounds of the map.
-//searchBox.setBounds(map.getBounds());
+// Create an infoWindow to display the details of the marker
+function populateInfoWindow(marker, infowindow) {
+  // Check if infowindow is already open for this marker. Allow only one infowindow at a time.
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.open(map, marker);
+    // Release the infowindow if clicked.
+    infowindow.addListener('closeclick', function() {
+      infowindow.marker = null;
+    });
+  }
+}
 
-// Define default and highlighted marker styles
-var defaultIcon = makeMarkerIcon('#082d68');
-var highlightedIcon = makeMarkerIcon('#ff0000');
+// Toggle visibility for all Markers in the Map. Adjust the map to ensure visbility of all markers.
+function toggleMarkers(visibility) {
+  var bounds = new google.maps.LatLngBounds();
 
-// InfoWindow to display marker details
-var largeInfowindow = new google.maps.InfoWindow();
+  for (var i = 0; i < markers.length; i++) {
+    if (visibility == 'show') {
+      markers[i].setMap(map);
+      bounds.extend(markers[i].position);
+      map.fitBounds(bounds);
+    }else {
+      markers[i].setMap(null);
+    }
+  }
+}
 
-// Initialize the locations for Neighbourhood
-var locations = [
-          {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-          {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-          {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-          {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-          {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-          {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-        ];
+// Allow only filtered marker to be visible. Adjust the map to ensure visbility.
+function filterMarker(title, visibility) {
+  var bounds = new google.maps.LatLngBounds();
+
+  for (var i = 0; i < markers.length; i++) {
+    if (markets[i].title == title) {
+      if (visibility == 'show') {
+        markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+        map.fitBounds(bounds);
+      }
+    }else {
+      markers[i].setMap(null);
+    }
+}
